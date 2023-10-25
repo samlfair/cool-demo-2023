306aeeb731448c0b5ec2e4b9a10137fd8112dbcd
Sam Littlefair <sam.littlefair@prismic.io>
2023-10-25 17:15:03 +0200

    debug cookies

--- src/routes/[[preview=preview]]/+page.server.js
+++ src/routes/[[preview=preview]]/+page.server.js
@@ -6,7 +6,7 @@ import { dev } from '$app/environment';
 export const prerender = false;
 
 export async function load({ fetch, cookies }) {
-	const client = createClient({ fetch });
+	const client = createClient({ cookies });
 	// const client = createClient({ fetch, cookies });
 
 	console.log();
